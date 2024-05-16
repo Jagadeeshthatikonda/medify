68050e26aad25741d6c6b721e491619ddc4c5b6b
jagadeesh <jagadeeshnoddy12@example.com>
2024-05-16 10:54:59 +0530

    Done adding navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import NavbarMedifyQuotation from "./components/NavbarMedifyQuotation/NavbarMedifyQuotation"
+import Navbar from "./components/Navbar/Navbar"
 import "./App.css";
 
 
 const App = () => {
 
   return (
-    <>
+    <div className=".App">
       <NavbarMedifyQuotation />
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          component={
+            <p>Sample component</p>
+          }
+        />
+      </Routes>
 
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+
+import "./styles.css";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <div className="header-container">
+      <img
+        src={logo}
+        onClick={() => navigate("/")}
+        height={27}
+        width={92}
+        alt="medify logo"
+      />
+
+      <NavigationLinks navigate={navigate} />
+
+    </div>
+  );
+};
+
+const NavigationLinks = ({ navigate }) => (
+  <nav className="nav-items-container">
+    <NavItem navigate={() => navigate("/findDoctor")}>Find Doctors</NavItem>
+    <NavItem>Hospitals</NavItem>
+    <NavItem>Medicines</NavItem>
+    <NavItem>Surgeries</NavItem>
+    <NavItem>Software for Provider</NavItem>
+    <NavItem>Facilities</NavItem>
+    <NavItemButton navigate={() => navigate("/myBookings")}>
+      My Bookings
+    </NavItemButton>
+  </nav>
+);
+
+const NavItem = ({ navigate, children }) => (
+  <span onClick={navigate}>{children}</span>
+);
+
+const NavItemButton = ({ navigate, children }) => (
+  <button onClick={navigate}>{children}</button>
+);
+
+export default Navbar;
