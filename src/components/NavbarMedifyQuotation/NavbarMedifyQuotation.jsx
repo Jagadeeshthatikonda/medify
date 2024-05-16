2ede155ae8d5278011e72d0c316c1e93e7c5dda0
jagadeesh <jagadeeshnoddy12@example.com>
2024-05-16 09:34:27 +0530

    Added medify header

---
+++ src/components/NavbarMedifyQuotation/NavbarMedifyQuotation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css";
+
+const NavbarMedifyQuotation = () => {
+  return (
+    <div className='quote-text'>
+      <p>The health and well-being of our patients and their health care team will always be our priority, so we follow the best practices for cleanliness.</p>
+    </div>
+  )
+}
+
+export default NavbarMedifyQuotation
