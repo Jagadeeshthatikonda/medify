85fb1ba66094b1dc71ec44f94e4814b8584a5901
jagadeesh <jagadeeshnoddy12@example.com>
2024-05-21 22:30:29 +0530

    Updated logo in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Logo from "../../assets/Logo.png";
+import Logo from "../assets/Logo.png";
 import "./styles.css";
 
 const Navbar = () => {
